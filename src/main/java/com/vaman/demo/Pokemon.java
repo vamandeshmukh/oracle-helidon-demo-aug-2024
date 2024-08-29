5860d64ff7a7fc2b2625dcacf514bbf80d896557
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-08-29 11:47:49 +0530

    initial commit

--- src/main/java/com/vaman/demo/Pokemon.java
+++ src/main/java/com/vaman/demo/Pokemon.java
@@ -6,6 +6,8 @@ import jakarta.persistence.AccessType;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.NamedQueries;
@@ -32,6 +34,7 @@ import jakarta.persistence.Transient;
 public class Pokemon {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 
 	private String name;
